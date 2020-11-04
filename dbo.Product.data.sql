4c2bbe2feca70da7cb12a80ce68d9e3e451f6881
pugazhendh <73930716+pugazhendh@users.noreply.github.com>
2020-11-04 13:56:57 +0530

    Initial commit

---
+++ dbo.Product.data.sql
@@ -0,0 +1,4 @@
+SET IDENTITY_INSERT [dbo].[Product] ON
+INSERT INTO [dbo].[Product] ([prodid], [productname], [proditeam], [Prodprice]) VALUES (1, N'Mobile', 10, 5000)
+INSERT INTO [dbo].[Product] ([prodid], [productname], [proditeam], [Prodprice]) VALUES (2, N'Datacard', 20, 60)
+SET IDENTITY_INSERT [dbo].[Product] OFF
